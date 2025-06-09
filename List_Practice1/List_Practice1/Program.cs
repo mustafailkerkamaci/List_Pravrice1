db85b282fe59208e5115a2daa0e92cc7d13eeed3
Mustafa İlker Kamaci <mustafailkerkmc@gmail.com>
2025-06-09 16:28:58 +0300

    Add files via upload

---
+++ List_Practice1/List_Practice1/Program.cs
@@ -0,0 +1,17 @@
+﻿List<string> visiters = new List<string>();
+
+visiters.Add("Bulent Ersoy ");
+visiters.Add("Ajda Pekkan");
+visiters.Add("Ebru Gundes");
+visiters.Add("Hadise");
+visiters.Add("Hande Yener");
+visiters.Add("Tarkan");
+visiters.Add("Funda Arar");
+visiters.Add("Demet Akalnin");
+
+Console.WriteLine("**DAVETLILER**");
+
+foreach (string visiter in visiters)
+{
+    Console.WriteLine(visiter);
+}
